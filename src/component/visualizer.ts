d862f3ffc13906eafa853240a67f10273e14678d
aketza <aketza.eguskiza@kaytek.io>
2026-07-24 21:58:29 +0200

    photos!

---
+++ src/component/visualizer.ts
@@ -0,0 +1,102 @@
+import { uiComponent } from "../lib/dom.js";
+import { Html } from "../lib/html.js";
+import { getIcon } from "../lib/icons.js";
+import { IconBundle, MaterialIcons } from "../model/configurations/icons.js";
+import { getImageUrl } from "../service/path.service.js";
+
+type albumType = {
+  [name: string]: string;
+};
+
+let image: HTMLImageElement;
+
+let currentPhoto: string;
+let currentAlbum: albumType;
+
+export function createVisualizer() {
+  const visualizer = uiComponent({
+    id: "visualizer"
+  });
+
+  const closeButton = getIcon(IconBundle.Material, MaterialIcons.Folder);
+  closeButton.id = "close";
+  closeButton.onclick = () => closeVisualizer();
+  visualizer.appendChild(closeButton);
+
+  // Note: Switched to ArrowCircleLeft for back button!
+  const backButton = getIcon(IconBundle.Material, MaterialIcons.Next, "3rem");
+  backButton.classList.add("button");
+  backButton.onclick = () => showLastPhoto(image.dataset.id);
+  visualizer.appendChild(backButton);
+
+  image = uiComponent({
+    type: Html.Img
+  }) as HTMLImageElement;
+  visualizer.appendChild(image);
+
+  const nextButton = getIcon(IconBundle.Material, MaterialIcons.Next, "3rem");
+  nextButton.classList.add("button");
+  nextButton.onclick = () => showNextPhoto(image.dataset.id);
+  visualizer.appendChild(nextButton);
+
+  return visualizer;
+}
+
+export function loadAlbum(album: albumType) {
+  currentAlbum = album;
+  const firstId = Object.keys(album)[0];
+  if (firstId) {
+    showPhoto(firstId, false);
+  }
+}
+
+export function loadAlbumAndPhoto(album: albumType, photoId: string) {
+  currentAlbum = album;
+  showPhoto(photoId);
+}
+
+export function showPhoto(currentId: string, show: boolean = true) {
+  if (!currentAlbum || !currentAlbum[currentId]) return;
+
+  image.dataset.id = currentId;
+  currentPhoto = currentAlbum[currentId];
+  image.src = getImageUrl(currentPhoto);
+
+  if (show) {
+    showVisualizer();
+  }
+}
+
+export function showNextPhoto(currentId: string) {
+  const keys = Object.keys(currentAlbum);
+  if (keys.length === 0) return;
+
+  const currentIndex = keys.indexOf(currentId);
+
+  if (currentIndex !== -1) {
+    const nextIndex = (currentIndex + 1) % keys.length;
+    const nextId = keys[nextIndex];
+    showPhoto(nextId);
+  }
+}
+
+export function showLastPhoto(currentId: string) {
+  const keys = Object.keys(currentAlbum);
+  if (keys.length === 0) return;
+
+  const currentIndex = keys.indexOf(currentId);
+
+  if (currentIndex !== -1) {
+    const prevIndex = (currentIndex - 1 + keys.length) % keys.length;
+    const prevId = keys[prevIndex];
+    showPhoto(prevId);
+  }
+}
+
+function closeVisualizer() {
+  document.getElementById("visualizer").classList.remove("show");
+}
+
+function showVisualizer() {
+  document.getElementById("visualizer").classList.add("show");
+}
--- src/start.ts
+++ src/start.ts
@@ -1,28 +1,13 @@
 import { listProjects } from "./command/list.projects.js";
-import {
-	clearTerminal,
-	getTerminalUI,
-	instanceTerminal,
-} from "./component/terminal.ui.js";
+import { clearTerminal, getTerminalUI, instanceTerminal } from "./component/terminal.ui.js";
 import { createTopBar } from "./component/top.bar.js";
 import Animations from "./lib/animations.js";
 import { BubbleUI } from "./lib/bubble.js";
-import {
-	getConfiguration,
-	isConfigurationActive,
-	isConfigurationSet,
-	loadConfiguration,
-	setConfiguration,
-} from "./lib/configuration.js";
+import { getConfiguration, isConfigurationActive, isConfigurationSet, loadConfiguration, setConfiguration } from "./lib/configuration.js";
 
 import { uiComponent } from "./lib/dom.js";
 import { loadIcons } from "./lib/icons.js";
-import {
-	setHomeRoute,
-	setNotFoundRoute,
-	setRoute,
-	showRoute,
-} from "./lib/router.js";
+import { setHomeRoute, setNotFoundRoute, setRoute, showRoute } from "./lib/router.js";
 import { sleep } from "./lib/time.js";
 import { AppConfigurations } from "./model/configurations/configurations.js";
 import { IconBundle } from "./model/configurations/icons.js";
@@ -33,6 +18,8 @@ import { showTerminalView } from "./view/terminal.view.js";
 import { checkDisplayType } from "./lib/display.js";
 import { showHomeView } from "./view/home.view.js";
 import { setCommands } from "./service/command.service.js";
+import { showPhotosView } from "./view/photos.view.js";
+import { setBackground } from "./service/ui.service.js";
 
 /**
  * When the dynamic URL changes loads
@@ -45,75 +32,70 @@ window.addEventListener("hashchange", start);
  * the app state to show
  */
 window.onload = async function () {
-	// Load configuration
-	await loadConfiguration("gtdf.config.json");
-	document.title = getConfiguration(AppConfigurations.AppName);
+  // Load configuration
+  await loadConfiguration("gtdf.config.json");
+  document.title = getConfiguration(AppConfigurations.AppName);
 
-	// Check ui start variables
-	checkDisplayType();
-	window.onresize = checkDisplayType;
-	checkAnimations();
-	await loadIcons(
-		IconBundle.Material,
-		`${getConfiguration("path")["icons"]}/materialicons.json`,
-	);
-	await loadIcons(
-		IconBundle.Social,
-		`${getConfiguration("path")["icons"]}/socialicons.json`,
-	);
+  // Check ui start variables
+  checkDisplayType();
+  window.onresize = checkDisplayType;
+  checkAnimations();
+  await loadIcons(IconBundle.Material, `${getConfiguration("path")["icons"]}/materialicons.json`);
+  await loadIcons(IconBundle.Social, `${getConfiguration("path")["icons"]}/socialicons.json`);
 
-	// Create basic DOM layout
-	const bar = createTopBar("akrck02.org");
-	document.body.appendChild(bar);
+  // Create basic DOM layout
+  const bar = createTopBar("akrck02.org");
+  document.body.appendChild(bar);
 
-	instanceTerminal();
-	const terminalUI = getTerminalUI();
-	clearTerminal();
-	sleep(1);
-	terminalUI.classList.add("show");
+  instanceTerminal();
+  const terminalUI = getTerminalUI();
+  clearTerminal();
+  sleep(1);
+  terminalUI.classList.add("show");
 
-	const content = uiComponent({
-		classes: [BubbleUI.BoxColumn],
-		id: "app-content",
-	});
-	content.appendChild(terminalUI);
-	document.body.appendChild(content);
+  const content = uiComponent({
+    classes: [BubbleUI.BoxColumn],
+    id: "app-content"
+  });
+  content.appendChild(terminalUI);
+  document.body.appendChild(content);
 
-	await start();
+  await start();
 };
 
 /**
  * Check if animations are enabled
  */
 function checkAnimations() {
-	// if it is the first time, enable animations by default
-	if (false === isConfigurationSet(AppConfigurations.Animations)) {
-		setConfiguration(AppConfigurations.Animations, true);
-	}
+  // if it is the first time, enable animations by default
+  if (false === isConfigurationSet(AppConfigurations.Animations)) {
+    setConfiguration(AppConfigurations.Animations, true);
+  }
 
-	Animations.enabled = isConfigurationActive(AppConfigurations.Animations);
+  Animations.enabled = isConfigurationActive(AppConfigurations.Animations);
 
-	// set the animation preference in all the document
-	if (Animations.enabled) document.documentElement.dataset.animations = "true";
+  // set the animation preference in all the document
+  if (Animations.enabled) document.documentElement.dataset.animations = "true";
 }
 
 /**
  *  Start the web app
  */
 async function start() {
-	setCommands();
-	setRoutes(getTerminalUI());
-	document.body.style.transition = "backgroundvar(--animation-slow)";
-	document.body.style.backgroundImage = ` url("${getConfiguration(AppConfigurations.Path)["images"]}/development.jpg")`;
+  document.body.style.transition = "background:var(--animation-slow)";
+  setBackground();
+  setCommands();
+  setRoutes(getTerminalUI());
 }
 
 /**
  * Set routes
  */
 function setRoutes(parent: HTMLElement) {
-	setHomeRoute(showHomeView);
-	setNotFoundRoute(showNotFoundView);
-	// setRoute("/projects", showProjectsView);
-	// setRoute("/terminal", showTerminalView);
-	showRoute(window.location.hash.slice(1).toLowerCase(), parent);
+  setHomeRoute(showHomeView);
+  setNotFoundRoute(showNotFoundView);
+  setRoute("/photos", showPhotosView);
+  // setRoute("/projects", showProjectsView);
+  // setRoute("/terminal", showTerminalView);
+  showRoute(window.location.hash.slice(1).toLowerCase(), parent);
 }
